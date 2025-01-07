9f09079adf672110345ad796093ea0e0827ead81
Snehal Srivastava <118104081+SnehalSrivastava27@users.noreply.github.com>
2025-01-07 22:24:23 +0530

    Footer Added

--- chrome/src/App.jsx
+++ chrome/src/App.jsx
@@ -2,13 +2,18 @@ import { Fragment } from "react";
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { Usage } from "./components/Usage";
+import { Footer } from "./components/Footer";
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 export default function App() {
   return (
+    <BrowserRouter>
   <Fragment>
     <Navbar></Navbar>
     <Hero></Hero>
     <Usage></Usage>
+    <Footer></Footer>
   </Fragment>
+  </BrowserRouter>
   )
 }
